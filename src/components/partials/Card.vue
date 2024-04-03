f28807b283ede9b5a777d1bb686c030a07529bb5
Daniele206 <danieledeieso2004@gmail.com>
2024-04-03 20:28:30 +0200

    [add] stars-for-vote [close] miletone 3

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -6,20 +6,68 @@
       language: String,
       vote: String,
       img: String
-    }
+    },
+
+    methods:{
+      getStars(num){
+        const stars = (num / 2).toFixed()
+        return stars
+      }
+    },
   }
 </script>
 
 
 <template>
   <div class="my-card position-relative" :style="`background-image: url(https://image.tmdb.org/t/p/w342${img}) ;`">
-    <div v-if="img === null" class="ico-not-img"m><i class="fa-solid fa-film"></i></div>
-    <div class="d-center flex-column info-card">
+    <div v-if="img === null" class="ico-not-img"><i class="fa-solid fa-film"></i></div>
+    <div class="d-center flex-column px-2 info-card">
       <div class="not-filter fs-3 fw-bold text-center">{{ title }}</div>
       <div class="not-filter fs-5 text-center">{{ originalTitle }}</div>
       <img v-if="language === 'it' || language === 'en'" class="not-filter w-25 m-2" :src="`/public/${language}.png`" :alt="language">
       <div v-else class="not-filter fs-5 fw-bold">{{ language }}</div>
-      <div class="not-filter fs-3 fw-bold">{{ vote }}</div>
+      <div v-if="getStars(vote) == 0" class="not-filter fs-3 fw-bold">
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+      </div>
+      <div v-else-if="getStars(vote) == 1" class="not-filter fs-3 fw-bold">
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+      </div>
+      <div v-else-if="getStars(vote) == 2" class="not-filter fs-3 fw-bold">
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+      </div>
+      <div v-else-if="getStars(vote) == 3" class="not-filter fs-3 fw-bold">
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+      </div>
+      <div v-else-if="getStars(vote) == 4" class="not-filter fs-3 fw-bold">
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-regular fa-star"></i>
+      </div>
+      <div v-else-if="getStars(vote) == 5" class="not-filter fs-3 fw-bold">
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+        <i class="fa-solid fa-star text-warning"></i>
+      </div>
     </div>
   </div>
 
@@ -33,6 +81,8 @@
     aspect-ratio: 10/14;
     overflow: hidden;
     color: white;
+    border: 1px solid white;
+    border-radius: 10px;
     background-color: rgb(201, 201, 201);
     background-size: cover;
     background-repeat: no-repeat;
@@ -44,7 +94,7 @@
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
-      font-size: 5rem;
+      font-size: 10rem;
     };
     &:hover .ico-not-img{
       z-index: -1;
