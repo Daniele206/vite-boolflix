22b7909a56bd6ebb3407d67b53398c001f0ddc0f
Daniele206 <danieledeieso2004@gmail.com>
2024-04-03 16:38:30 +0200

    [add] getApi()

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
   import axios from 'axios';
   //axios
 
+  // store
+  import { store } from './data/store';
+  // store
+
   import Header from './components/Header.vue';
   import Main from './components/Main.vue';
   import Footer from './components/Footer.vue';
@@ -13,7 +17,30 @@
       Main,
       Footer
     },
+
+    data(){
+      return{
+        store
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(this.store.apiUrl, {
+          params :this.store.respParams
+        })
+        .then(result => {
+          console.log(result.data.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+    },
     
+    mounted(){
+      this.getApi()
+    }
   }
 </script>
 
